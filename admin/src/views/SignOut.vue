31f16ed5254a07f6054771f02e37d11757b49156
fengluo <fengluo17@gmail.com>
2017-02-10 21:47:34 +0800

    fix: fix signout

--- admin/src/views/SignOut.vue
+++ admin/src/views/SignOut.vue
@@ -6,7 +6,7 @@
         name: 'SignOut',
         created: function(){
             localStorage.removeItem('auth')
-            this.$route.router.push({name: 'SignIn'})
+            this.$router.push({name: 'SignIn'})
         }
     }
 </script>
