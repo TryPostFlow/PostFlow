1e593d04ba58fe61f2b6ac6cb286d372f84daeb2
fengluo <fengluo17@gmail.com>
2017-02-16 15:04:15 +0800

    remove flask-cache

--- planet/extensions.py
+++ planet/extensions.py
@@ -2,15 +2,13 @@
 # coding=utf-8
 
 from flask_mail import Mail
-from flask_cache import Cache
 from flask_sqlalchemy import SQLAlchemy
 from flask_principal import Principal
 from flask_oauthlib.provider import OAuth2Provider
 
-__all__ = ['mail', 'db', 'cache', 'principals', 'oauth']
+__all__ = ['mail', 'db', 'principals', 'oauth']
 
 mail = Mail()
 db = SQLAlchemy()
-cache = Cache()
 principals = Principal(use_sessions=False)
 oauth = OAuth2Provider()
