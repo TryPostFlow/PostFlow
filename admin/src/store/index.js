399ace7ed805f7562482da2e939cdb1154a61ffd
fengluo <fengluo17@gmail.com>
2017-03-14 22:58:01 +0800

    feat: add logger when development mode

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -6,7 +6,7 @@ import { Post, Tag, Account, Setting } from './modules'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  plugins: [createLogger()],
+  plugins: process.env.NODE_ENV === 'production'?[]:[createLogger()],
   modules: {
     Account,
     Post,
