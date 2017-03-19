0177b71e9fc651ab3b539cbe8485525d741c3b1b
fengluo <fengluo17@gmail.com>
2017-03-19 23:01:27 +0800

    style: remove unused file

--- planet/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from werkzeug.contrib.fixers import ProxyFix
-from planet import create_app
-
-app = create_app()
-# app.wsgi_app = ProxyFix(app.wsgi_app)
