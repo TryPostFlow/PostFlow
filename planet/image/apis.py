644857216891e62148448619e8fc9f1aebfa6dfa
fengluo <fengluo17@gmail.com>
2017-03-26 16:54:51 +0800

    feat: add permissions to image module

--- planet/image/apis.py
+++ planet/image/apis.py
@@ -7,10 +7,14 @@ import time
 from flask import request, jsonify
 from werkzeug.utils import secure_filename
 from planet.extensions import storage
+from planet.utils.permissions import auth
 from planet.image import image_api
+from planet.image.permissions import image_upload_perm
 
 
 @image_api.route('', methods=["POST"])
+@auth.require(401)
+@image_upload_perm.require(403)
 def upload():
     image_data = request.files.get('image')
 
