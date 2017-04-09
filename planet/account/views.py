f3421f6ef0495cf27bc363f7d4097542261eddf7
fengluo <fengluo17@gmail.com>
2017-04-09 23:00:38 +0800

    fix: abort 404

--- planet/account/views.py
+++ planet/account/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from flask import abort
 from planet.account import account_view
 from planet.account.models import get_posts_by_user, get_user
 
@@ -10,6 +11,7 @@ from planet.helpers.template import render_template
 @account_view.route('/author/<slug>/page/<int:page>', methods=['GET'])
 def show(slug, page=1):
     user = get_user(slug)
+    if not user:
+        abort(404)
     posts = get_posts_by_user(user.id, page)
-    return render_template(
-        'author.html', author=user, posts=posts)
+    return render_template('author.html', author=user, posts=posts)
--- planet/post/views.py
+++ planet/post/views.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask import g, request, url_for
+from flask import g, request, url_for, abort
 from werkzeug.contrib.atom import AtomFeed
 from planet.utils.schema import render_schema
 from planet.helpers.template import render_template
 from planet.post import post_view
-from planet.post.models import (
-    get_all_posts, get_post, get_next_post,
-    get_prev_post, add_post_views, Post)
-
+from planet.post.models import (get_all_posts, get_post, get_next_post,
+                                get_prev_post, add_post_views, Post)
 
 
 @post_view.route('/', methods=['GET'])
@@ -43,16 +41,15 @@ def feed():
 def hit(slug):
     post = get_post(slug)
     add_post_views(post.id)
-    message = {
-        'code': 10000,
-        'message': 'success'
-    }
+    message = {'code': 10000, 'message': 'success'}
     return render_schema(message)
 
 
 @post_view.route('/<path:slug>', methods=['GET'])
 def show(slug):
     post = get_post(slug)
+    if not post:
+        abort(404)
     next_post = get_next_post(post.id)
     prev_post = get_prev_post(post.id)
     return render_template(
