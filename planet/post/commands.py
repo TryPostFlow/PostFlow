62efe91c7add69ece2cf967eacb5843a6033ddd4
fengluo <fengluo17@gmail.com>
2017-04-09 21:11:49 +0800

    fix: insert_sample_post

--- planet/post/commands.py
+++ planet/post/commands.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from planet.extensions import db
 from planet.post.fixtures import SAMPLE_POST
 from planet.post.schemas import PostSchema
+from planet.account.models import Role, User
 
 
 def insert_sample_post():
+    user_data = User.query\
+        .join(Role, Role.id == User.primary_role_id)\
+        .filter(Role.slug == 'owner').first()
+    if not user_data:
+        user_data = User.query.first()
+    if not user_data:
+        return
     post_data = PostSchema().load(SAMPLE_POST).data
+    post_data.published_at = datetime.utcnow()
+    post_data.published_by = user_data.id
+    post_data.created_by = user_data.id
+    post_data.updated_by = user_data.id
     db.session.add(post_data)
     db.session.commit()
     return post_data
