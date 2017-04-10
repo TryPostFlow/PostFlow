629c4a3c2d3923a79c4dfc3967b4776af032116e
fengluo <fengluo17@gmail.com>
2017-04-10 22:44:01 +0800

    fix: theme download url

--- planet/commands/theme.py
+++ planet/commands/theme.py
@@ -16,14 +16,17 @@ def theme():
     """Create, update or delete theme."""
     pass
 
+
 def download_theme(path):
     theme_name = path.split('/')[-1]
     themes_folder = os.path.join(current_app.instance_path, 'content/themes')
     theme_path = os.path.join(themes_folder, theme_name)
 
     if os.path.exists(theme_path):
-        if click.confirm(click.style(
-                "Existing theme found. Do you want to override it?", fg="magenta")):
+        if click.confirm(
+                click.style(
+                    "Existing theme found. Do you want to override it?",
+                    fg="magenta")):
             shutil.rmtree(theme_path)
         else:
             return theme_name
@@ -36,7 +39,7 @@ def download_theme(path):
         response = requests.get(link, stream=True)
         total_length = response.headers.get('content-length')
 
-        if total_length is None: # no content length header
+        if total_length is None:  # no content length header
             f.write(response.content)
         else:
             dl = 0
@@ -45,7 +48,7 @@ def download_theme(path):
                 dl += len(data)
                 f.write(data)
                 done = int(50 * dl / total_length)
-                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
                 sys.stdout.flush()
             sys.stdout.write("\n")
 
@@ -57,14 +60,18 @@ def download_theme(path):
         os.path.join(themes_folder, repo_dir),
         os.path.join(themes_folder, theme_name))
     os.remove(file_path)
-    sys.stdout.write("[+] {} has been successfully downloaded!\n".format(theme_name))
+    sys.stdout.write(
+        "[+] {} has been successfully downloaded!\n".format(theme_name))
     return theme_name
+
 
 @theme.command("download")
 @click.option("--path", "-u", help="The github path of the theme.")
 def download(path):
     if not path:
         path = click.prompt(
-            click.style("Theme name (Github: user_name/repo_name)", fg="magenta"), type=str,
-            default="fengluo/casper")
+            click.style(
+                "Theme name (Github: user_name/repo_name)", fg="magenta"),
+            type=str,
+            default="TryPostFlow/Casper")
     download_theme(path)
