4803361af17327e1cba0b8be3aa2e57316322335
fengluo <fengluo17@gmail.com>
2017-04-14 10:37:40 +0800

    chore: miss package

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
     install_requires=[
         'feedparser==5.2.1', 'Flask==0.12', 'Flask-Mail==0.9.1',
         'Flask-OAuthlib==0.9.3', 'Flask-Principal==0.4.0',
-        'Flask-SQLAlchemy==2.1', 'Flask-Themes2==0.1.4', 'gevent==1.1.1',
+        'Flask-Alembic==2.0.1', 'Flask-SQLAlchemy==2.1',
+        'Flask-Themes2==0.1.4', 'SQLAlchemy-Utils==0.32.12', 'gevent==1.1.1',
         'gunicorn==19.6.0', 'jieba==0.38', 'Jinja2==2.9.5',
         'marshmallow==2.13.4', 'mistune==0.7.1', 'nose==1.3.7',
         'pypinyin==0.16.1', 'python-dateutil==2.6.0', 'simplejson==3.10.0',
