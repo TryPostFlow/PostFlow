300d598f7cfd565a14eb872c410399c7fbd8342a
fengluo <fengluo17@gmail.com>
2017-05-04 14:43:47 +0800

    feat: update post module

--- postflow/post/views.py
+++ postflow/post/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask import g, request, url_for, abort
+from flask import g, request, url_for, jsonify
 from werkzeug.contrib.atom import AtomFeed
-from postflow.utils.schema import render_schema
+from postflow.extensions import db
 from postflow.helpers.template import render_template
 from postflow.post import post_view
-from postflow.post.models import (get_all_posts, get_post, get_next_post,
-                                get_prev_post, add_post_views, Post)
+from postflow.post.models import (get_all_posts, get_next_post, get_prev_post,
+                                  Post)
 
 
 @post_view.route('/', methods=['GET'])
@@ -39,18 +39,18 @@ def feed():
 
 @post_view.route('/<path:slug>/views', methods=['POST'])
 def hit(slug):
-    post = get_post(slug)
-    add_post_views(post.id)
-    message = {'code': 10000, 'message': 'success'}
-    return render_schema(message)
+    post_data = Post.query.filter(
+        db.or_(Post.id == slug, Post.slug == slug)).first_or_404()
+    post_data.views += 1
+    post_data.save()
+    return jsonify(code=10000, message='success')
 
 
 @post_view.route('/<path:slug>', methods=['GET'])
 def show(slug):
-    post = get_post(slug)
-    if not post:
-        abort(404)
-    next_post = get_next_post(post.id)
-    prev_post = get_prev_post(post.id)
+    post_data = Post.query.filter(
+        db.or_(Post.id == slug, Post.slug == slug)).first_or_404()
+    next_post = get_next_post(post_data.id)
+    prev_post = get_prev_post(post_data.id)
     return render_template(
-        'post.html', post=post, next_post=next_post, prev_post=prev_post)
+        'post.html', post=post_data, next_post=next_post, prev_post=prev_post)
