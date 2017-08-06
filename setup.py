b6904953617cf577622ba5f276e0d9308bd0224b
fengluo <fengluo17@gmail.com>
2017-08-06 21:27:12 +0800

    release: fix async task

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='postflow',
-    version='0.1.3',
+    version='0.1.4',
     author='Shawn Xie',
     author_email='fengluo17@gmail.com',
     url="https://github.com/TryPostFlow/PostFlow",
