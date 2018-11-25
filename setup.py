4c5a9862d74e3c5cb6b2b04ea16ab50af60de623
Shawn Xie <fengluo17@gmail.com>
2018-11-25 19:41:58 +0800

    fix: update marshmallow version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Flask-Alembic==2.0.1', 'Flask-SQLAlchemy==2.1',
         'Flask-Themes2==0.1.4', 'SQLAlchemy-Utils==0.32.12', 'gevent==1.1.1',
         'gunicorn==19.6.0', 'jieba==0.38', 'Jinja2==2.9.5',
-        'marshmallow==2.13.4', 'mistune==0.7.1', 'nose==1.3.7',
+        'marshmallow==2.16.3', 'mistune==0.7.1', 'nose==1.3.7',
         'pypinyin==0.16.1', 'python-dateutil==2.6.0', 'simplejson==3.10.0',
         'Unidecode==0.4.20', 'raven==6.0.0'
     ])
